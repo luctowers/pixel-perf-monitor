1b5f8a78f3536282a82090cbdf98a048ade20aa8
Lucas Towers <luctowers@gmail.com>
2020-10-25 20:10:55 -0700

    Make indentation and newlines consistent

--- controller/main.py
+++ controller/main.py
@@ -6,10 +6,10 @@ from widgets import CpuWidget, GpuWidget
 
 displayio.release_displays()
 matrix = RGBMatrix(
-    width=32, height=32, bit_depth=4,
-    rgb_pins=[board.D6, board.D5, board.D9, board.D11, board.D10, board.D12],
-    addr_pins=[board.A5, board.A4, board.A3, board.A2],
-    clock_pin=board.D13, latch_pin=board.D0, output_enable_pin=board.D1
+  width=32, height=32, bit_depth=4,
+  rgb_pins=[board.D6, board.D5, board.D9, board.D11, board.D10, board.D12],
+  addr_pins=[board.A5, board.A4, board.A3, board.A2],
+  clock_pin=board.D13, latch_pin=board.D0, output_enable_pin=board.D1
 )
 display = FramebufferDisplay(matrix, auto_refresh=False)
 
@@ -21,8 +21,8 @@ g.append(gpu)
 display.show(g)
 
 while True:
-    for pu in [cpu, gpu]:
-        pu.activity = (pu.activity + 0.003) % 1.0
-        pu.memory_usage = (pu.memory_usage + 0.02) % 1.0
-        pu.temperature = (pu.temperature + 1) % 100
-    display.refresh(target_frames_per_second=10)
+  for pu in [cpu, gpu]:
+    pu.activity = (pu.activity + 0.003) % 1.0
+    pu.memory_usage = (pu.memory_usage + 0.02) % 1.0
+    pu.temperature = (pu.temperature + 1) % 100
+  display.refresh(target_frames_per_second=10)
